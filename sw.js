005c8b67987e02c53e4065959b78f1cb2c88a7d9
trailtropical <trailtropicalpodcast@gmail.com>
2026-07-16 13:41:15 -0300

    feat: botao baixar app com modal android/ios + service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'csd-v1'
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-csd-quad.jpg'
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+})
